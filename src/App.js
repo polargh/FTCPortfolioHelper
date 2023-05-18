88824e28dc052ba58c1b1efbced0fdb3b9f8221e
ayaamghi <ayaamghi@gmail.com>
2023-05-18 09:13:59 -0400

    git stash but git stash is hard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "./Components/Footer"
 import PortSearch from "./Components/PortSearch";
+import Team from "./Components/Team"
 function App() {
   return (
+    
       <><div className="flex flex-col h-screen justify-between">
+      
         <PortSearch /> 
-    </div><div>
+    </div>
+    <div>
         <Footer />
 
 
---
+++ src/Portfolio.js
@@ -0,0 +1,20 @@
+class Portfolio { 
+    constructor(url, season, level, highestAward) { 
+        this.url = url; 
+        this.season = season; 
+        this.level = level;
+        this.awards = highestAward
+    }
+    getUrl() { 
+        return url; 
+    }
+    getSeason() { 
+        return this.season; 
+    }
+    getLevel() { 
+        return level; 
+    }
+   // getHighestAward() { 
+  //      return 
+  //  }
+}
